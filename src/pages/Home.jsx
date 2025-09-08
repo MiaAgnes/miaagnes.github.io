2c48f438c53e547b0c464f15a9e0bba731648c8b
MiaAgnes <eaa24matd@students.eaaa.dk>
2025-09-08 23:15:27 +0200

    Testing workflow

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
             <Header />
             <h1>Velkommen til min portfolio</h1>
             <p>Dette er forsiden af min portfolio.</p>
+            <p>Hvis denne text vises virker min github action / workflow.</p>
         </div>
     );
 }
