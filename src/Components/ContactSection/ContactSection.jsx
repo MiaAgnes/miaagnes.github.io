a1be4e966ec67196a2421046e6e8eaf734ba5454
MiaAgnes <eaa24matd@students.eaaa.dk>
2025-09-13 20:19:11 +0200

    ContactSection

--- src/Components/ContactSection/ContactSection.jsx
+++ src/Components/ContactSection/ContactSection.jsx
@@ -2,9 +2,18 @@ import styles from "./ContactSection.module.css";
 
 export default function ContactSection() {
   return (
-    <div className={styles.contactSectionBg}  >
-      <h2>Contact Me</h2>
-      <p>This is the contact section.</p>
-    </div>
+    <section className={styles.contactSectionBg}  >
+      <div className={styles.contactSectionText}>
+        <h2>Get in touch</h2>
+        <div className={styles.contactSectionContent}>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis neque incidunt sapiente magni aliquid necessitatibus nisi quae, suscipit molestias eligendi laudantium deleniti adipisci, obcaecati praesentium perspiciatis enim? Commodi, numquam dolore?</p>
+        </div>
+      </div>
+      <div className={styles.tapeColumn}>
+        <img className={styles.tape} src="/images/tape.png" alt="tape" />
+        <img className={styles.tape} src="/images/tape.png" alt="tape" />
+        <img className={styles.tape} src="/images/tape.png" alt="tape" />
+      </div>
+    </section>
   );
 }
